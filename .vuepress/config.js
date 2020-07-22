cd66395b49936a552e0e89c15715c4ab1362cd76
Pei-Ming Wu <p408865@gmail.com>
2020-07-22 08:42:01 +0800

    feat: add avatar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,16 +5,18 @@ module.exports = {
   theme: 'reco',
   themeConfig: {
     type: 'blog',
-    tags: true,
     // Ref: https://vuepress-theme-reco.recoluan.com/en/views/1.x/timeline.html#add-navigation-button
     nav: [
       { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
       { text: 'Github', link: 'https://github.com/mkfsn/blog', icon: 'reco-github' }
     ],
+    // Ref: https://vuepress-theme-reco.recoluan.com/en/views/1.x/blog.html#add-blog-configurations
     blogConfig: {
       category: {location: 1, text: "Category"},
       tag: {location: 2, text: "Tag"}
     },
+    // Ref: https://vuepress-theme-reco.recoluan.com/en/views/1.x/home.html#home-blog
+    authorAvatar: '/avatar.png',
   },
   markdown: {
     // options for markdown-it-anchor
